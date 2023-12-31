b29c5a83bd8fc977d8baa90e5a20ec3bb958b1ad
Tsotneshonia1 <cotneshonia.17@gmail.com>
2023-12-31 16:14:31 +0400

    site-ubdate

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,7 @@ function About() {
       <div className={styles.imgContainer}>
         <Image
           src={
-            "https://www.pexels.com/photo/fingers-on-black-computer-keyboard-7848988/"
+            "https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           }
           fill={true}
           all="keyboard"
